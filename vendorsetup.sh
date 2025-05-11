e95f425b990a71710736b9607b6eb37c3708ba75
JYRRC <javixhtsweet@gmail.com>
2025-05-11 14:44:13 +0200

    garnet: modify vendorsetup
    Signed-off-by: JYRRC <javixhtsweet@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,34 @@
+echo 'Starting to clone stuffs needed to build for garnet'
+
+# Vendor
+echo 'Cloning Vendor'
+git clone https://github.com/Garnet-DarkSide/vendor_xiaomi_garnet.git -b alpha-15.2 vendor/xiaomi/garnet
+
+# Kernel
+echo 'Cloning Kernel'
+git clone https://github.com/Garnet-DarkSide/device_xiaomi_garnet-prebuilt.git -b kyoka-ksu device/xiaomi/garnet-prebuilt
+
+# Hardware
+echo 'Cloning Hardware'
+git clone https://github.com/Garnet-DarkSide/hardware_xiaomi.git -b 15 hardware/xiaomi
+
+# Dolby
+echo 'Cloning Dolby'
+git clone https://github.com/Garnet-DarkSide/hardware_dolby.git -b 15 hardware/dolby
+
+# Miuicamera
+echo 'Cloning Camera Device'
+git clone https://github.com/Garnet-DarkSide/device_xiaomi_miuicamera-garnet.git -b 15.2 device/xiaomi/miuicamera-garnet
+
+# Camera
+echo 'Cloning Camera Vendor'
+git clone https://codeberg.org/JYRRC/vendor_xiaomi_miuicamera-garnet.git -b 15 vendor/xiaomi/miuicamera-garnet
+
+# Basic Call Recorder (bcr)
+echo 'Cloning Bcr'
+git clone https://github.com/Garnet-DarkSide/vendor_bcr.git vendor/bcr
+
+# Audio
 rm -rf hardware/qcom-caf/sm8450/audio/graphservices/*
 
 cd hardware/qcom-caf/sm8450/audio/agm
